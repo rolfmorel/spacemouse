db457904e8dbfb08c4ef8a1d05366d0ad643c710
polyphemus <rolfmorel@gmail.com>
2013-12-23 14:52:08 +0100

    Add extended version info

--- VERSION.mk
+++ VERSION.mk
@@ -1,1 +1,14 @@
-VERSION = 0.1
+VER_MAJOR = 0
+VER_MINOR = 1
+VER_PATCH =
+
+ifneq ($(VER_PATCH),)
+VERSION = $(VER_MAJOR).$(VER_MINOR).$(VER_PATCH)
+else
+VERSION = $(VER_MAJOR).$(VER_MINOR)
+endif
+
+all:
+
+version:
+	$(info $(VERSION))
